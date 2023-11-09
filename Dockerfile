3de091732ccdce6ea5b628037cb2ebce3ce1b956
bugsssssss <n.soatov@bk.ru>
2023-11-10 00:47:48 +0500

    db changes to postgres

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ COPY server /server
 WORKDIR /server
 EXPOSE 8000
 
-# RUN apk add postgresql-client build-base postgresql-dev
+RUN apk add postgresql-client build-base postgresql-dev
 
 RUN pip install -r /temp/requirements.txt
 
--- server/server/settings.py
+++ server/server/settings.py
@@ -71,12 +71,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 print("DB_HOST:", os.environ.get('DB_HOST'))
 print("DB_NAME:", os.environ.get('DB_NAME'))
@@ -84,15 +84,15 @@ print("DB_USER:", os.environ.get('DB_USER'))
 print("DB_PASS:", os.environ.get('DB_PASS'))
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'HOST': os.environ.get('DB_HOST'),
-#         'NAME': os.environ.get('DB_NAME'),
-#         'USER': os.environ.get('DB_USER'),
-#         'PASSWORD': os.environ.get('DB_PASS'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+    }
+}
 
 
 # Password validation
